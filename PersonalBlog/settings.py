5d6fc646a57aab6bdf1d756d22597bf416c39ab9
Udoy <srudoy436@gmail.com>
2021-11-23 20:49:15 +0600

    new change in django setting

--- PersonalBlog/settings.py
+++ PersonalBlog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-my9d!*xgu#yo=m!uw=0!%ad1e22lsf$7#lf4776#$pz_tgrzrm
 DEBUG = False
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','udoyrahman.herokuapp.com']
 
 
 # Application definition
